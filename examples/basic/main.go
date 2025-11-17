8b43cb28044e7f32bd34a99277138a0e98d5a99a
PavelKhromykh <pavel.khromykh.go@yandex.ru>
2025-11-17 23:44:43 +0300

    add example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"os"
+
+	"github.com/PavelKhromykhGo/fastio/fastio"
+)
+
+func main() {
+	fr := fastio.NewReader(os.Stdin)
+	fw := fastio.NewWriter(os.Stdout)
+	defer fw.Flush()
+
+	n, err := fr.NextInt()
+	if err != nil {
+		return
+	}
+
+	sum := 0
+	for i := 0; i < n; i++ {
+		x, err := fr.NextInt()
+		if err != nil {
+			return
+		}
+		sum += x
+	}
+
+	_ = fw.WriteInt(sum)
+	_ = fw.WriteByte('\n')
+}
